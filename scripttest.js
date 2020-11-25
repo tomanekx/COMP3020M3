02cb6a5fdb3e89f8afc6282d09112887dfc610cc
tomanekx <73558525+tomanekx@users.noreply.github.com>
2020-11-25 12:18:53 -0600

    Delete scripttest.js

--- scripttest.js
+++
@@ -1,8 +0,0 @@
-function swapStyleSheet(sheet){
-    document.getElementById('pagestyle').setAttribute('href', sheet);
-
-}
-
-document.getElementById("matches").onclick = function() {
-    location.href = "matches.html";
-}

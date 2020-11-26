1f1c02ed0dd76f90847ad8b352109d72784da6e8
tomanekx <73558525+tomanekx@users.noreply.github.com>
2020-11-26 00:27:10 -0600

    Add files via upload

--- Restaurant.js
+++ Restaurant.js
@@ -57,7 +57,7 @@ $(document).ready(function(){
 	var actions = $("table td:last-child").html();
 	// Append table with add row form on add new button click
     $(".add-new").click(function(){
-		$(this).attr("disabled", "disabled");
+		//$(this).attr("disabled", "disabled");
 		var index = $("table tbody tr:last-child").index();
         var row = '<tr>' +
             '<td>Item Name</td>' +
@@ -67,29 +67,9 @@ $(document).ready(function(){
         '</tr>';
     	$("table").append(row);		
 		$("table tbody tr").eq(index + 1);//.find(".add, .edit").toggle();
-        $('[data-toggle="tooltip"]').tooltip();
+        
     });
-	// Add row on add button click
-	$(document).on("click", ".add", function(){
-		var empty = false;
-		var input = $(this).parents("tr").find('input[type="text"]');
-        input.each(function(){
-			if(!$(this).val()){
-				$(this).addClass("error");
-				empty = true;
-			} else{
-                $(this).removeClass("error");
-            }
-		});
-		$(this).parents("tr").find(".error").first().focus();
-		if(!empty){
-			input.each(function(){
-				$(this).parent("td").html($(this).val());
-			});			
-			$(this).parents("tr").find(".add, .edit").toggle();
-			$(".add-new").removeAttr("disabled");
-		}		
-    });
+	
 	// Edit row on edit button click
 	$(document).on("click", ".edit", function(){		
         $(this).parents("tr").find("td:not(:last-child)").each(function(){

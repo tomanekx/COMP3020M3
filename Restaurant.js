9c28ed37e0a94712b6f0a2b21dc1f19a845ec7a5
tomanekx <73558525+tomanekx@users.noreply.github.com>
2020-11-27 01:02:08 -0600

    Add files via upload

--- Restaurant.js
+++ Restaurant.js
@@ -24,32 +24,88 @@ $(document).ready(function() {
     var addVal = 0.0;
     var newVal = 0.0;
     var prevVal = 0.0; 
+    
     sessionStorage.setItem("currItem", 10.00);
     $("#addCart b").html(sessionStorage.getItem("currItem"));
 
     $("#option").change(function() {
         addVal = Number($("input[name=options]:checked").val());
         newVal = addVal - prevVal + Number(sessionStorage.getItem("currItem"));
+        prevVal = addVal;
         sessionStorage.setItem("currItem", newVal);
         $("#addCart b").html(sessionStorage.getItem("currItem"));
-    })
-
+    });
+    var check1 = false; 
     $("#addon1").click(function() {
+        
         addVal = Number($("#addon1").val());
-        if ($("#addon1").is("checked")) {
+        if (!check1) {
             newVal = addVal + Number(sessionStorage.getItem("currItem"));
             sessionStorage.setItem("currItem", newVal);
+            check1 = true; 
         } else {
-            newVal = addVal - Number(sessionStorage.getItem("currItem"));
+            newVal = - addVal + Number(sessionStorage.getItem("currItem"));
             sessionStorage.setItem("currItem", newVal);
+            check1 = false; 
         }
 
         $("#addCart b").html(sessionStorage.getItem("currItem"));
 
-    })
+    });
+    var check2 = false; 
+    $("#addon2").click(function() {
+        
+        addVal = Number($("#addon2").val());
+        if (!check2) {
+            newVal = addVal + Number(sessionStorage.getItem("currItem"));
+            sessionStorage.setItem("currItem", newVal);
+            check2 = true; 
+        } else {
+            newVal = - addVal + Number(sessionStorage.getItem("currItem"));
+            sessionStorage.setItem("currItem", newVal);
+            check2 = false; 
+        }
+
+        $("#addCart b").html(sessionStorage.getItem("currItem"));
+
+    });
+    var check3 = false; 
+    $("#addon3").click(function() {
+        
+        addVal = Number($("#addon3").val());
+        if (!check3) {
+            newVal = addVal + Number(sessionStorage.getItem("currItem"));
+            sessionStorage.setItem("currItem", newVal);
+            check3 = true; 
+        } else {
+            newVal = - addVal + Number(sessionStorage.getItem("currItem"));
+            sessionStorage.setItem("currItem", newVal);
+            check3 = false; 
+        }
+
+        $("#addCart b").html(sessionStorage.getItem("currItem"));
+
+    });
+    var check4 = false; 
+    $("#addon4").click(function() {
+        
+        addVal = Number($("#addon4").val());
+        if (!check4) {
+            newVal = addVal + Number(sessionStorage.getItem("currItem"));
+            sessionStorage.setItem("currItem", newVal);
+            check4 = true; 
+        } else {
+            newVal = - addVal + Number(sessionStorage.getItem("currItem"));
+            sessionStorage.setItem("currItem", newVal);
+            check4 = false; 
+        }
+
+        $("#addCart b").html(sessionStorage.getItem("currItem"));
+
+    });
 
 
-})
+});
 
 
 
@@ -59,12 +115,13 @@ $(document).ready(function() {
     // Append table with add row form on add new button click
     $(".add-new").click(function() {
         //$(this).attr("disabled", "disabled");
+        var price = sessionStorage.getItem("currItem");
         var index = $("table tbody tr:last-child").index();
         var row = '<tr>' +
-            '<td>' + actions + '</td>' +
+            '<td>' + actions + '   </td>' +
             '<td>Item Name</td>' +
             '<td>1</td>' +
-            '<td> $15.00 </td>' +
+            '<td>$' + price + '</td>' +
 
             '</tr>';
         $("table").append(row);
